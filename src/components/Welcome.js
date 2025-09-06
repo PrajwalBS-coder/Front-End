c8dd5438e15c0242d82fe1bbcbdbe347655e7f49
Prajwal B S <prajwaljhon@gmail.com>
2025-09-06 12:35:29 +0530

    Add Welcome component and integrate it into the App

---
+++ src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React ,{Component} from "react";
+
+class Welcome extends Component {
+    render() {
+        return <h1>Welcome to the Detection Microfrontend. This is the Welcome Component</h1>;
+    }
+}
+export default Welcome;
