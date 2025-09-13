b388ec7e4f0ce95788488f7f4e947cb407b118d7
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-09-13 19:53:17 +0530

    feat: refactor increment logic to separate method with state callback
    Extract inline increment logic to dedicated method with setState callback to demonstrate asynchronous state update behavior and improve code organization

--- src/components/CountNo.js
+++ src/components/CountNo.js
@@ -7,12 +7,21 @@ class CountNo extends Component {
       count: 0,
     };
   }
+  increment() {
+    this.setState({ count: this.state.count + 1 }, () =>
+      console.log(this.state.count)//Here The Value will print after increment
+    );
+    console.log(this.state.count);//Here The Value will print first then increment
+  }
+
   render() {
-    return (<div>
+    return (
+      <div>
         count{this.state.count}
         <br></br>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>Increment</button>
-    </div>);
+        <button onClick={() => this.increment()}>Increment</button>
+      </div>
+    );
   }
 }
 
